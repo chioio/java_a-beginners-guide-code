16db4489555b554072801557c4cc64b6e40b11ff
chioio <Tenn_Chio@outlook.com>
2018-12-30 22:51:54 +0800

    Create project structure.

---
+++ src/main/java/com/chio/_1_java_fundamentals/Example.java
@@ -0,0 +1,19 @@
+package com.chio._1_java_fundamentals;
+
+/*
+ * This is a simple Java program.
+ * Call this file Example.java
+ */
+public class Example {
+    // A Java program begins with a call to main().
+    public static void main(String[] args) {
+        System.out.println("Java divers the Web.");
+    }
+}
+
+/*
+ * Follow these three steps:
+ * 1.Enter the program.
+ * 2.Compile the program.
+ * 3.Run the program.
+ */
